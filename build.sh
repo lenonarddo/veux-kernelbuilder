70cfb221e65709aad2e1cacf1810c902d0dbf992
Lenonard Do <sangquang1202@gmail.com>
2025-08-01 18:14:33 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -36,13 +36,11 @@ getsource () {
     git clone --depth=1 $KERNEL_SOURCE $KBRANCH common
     set +x
     fi
-    if [ ! -d "common/kernel/msm-5.4" ]; then
     set -x
     echo Patch MSM-5.4
     cd common/kernel/
-    git clone --depth=1 https://github.com/sunscape-stuff/msm-5.4
+    git clone --depth=1 https://github.com/sunscape-stuff/msm-5.4 msm-5.4
     cd ../../
-    fi
     set +x
 }
 gettools () {
